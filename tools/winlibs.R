a5d82c3ca0864adfcdc4c55f892d017d785e0035
Mauricio 'Pacha' Vargas Sepulveda <m.sepulveda@mail.utoronto.ca>
2024-12-08 21:58:26 -0500

    add harfbuzz to windows setup

--- tools/winlibs.R
+++ tools/winlibs.R
@@ -1,16 +1,35 @@
-if(!file.exists("../windows/poppler/include/poppler/cpp/poppler-document.h")){
+# Poppler ----
+
+if (!file.exists("../windows/poppler/include/poppler/cpp/poppler-document.h")) {
   unlink("../windows", recursive = TRUE)
-  url <- if(grepl("aarch", R.version$platform)){
+  url <- if (grepl("aarch", R.version$platform)) {
     "https://github.com/pachadotdev/cpp11bundles/releases/download/poppler-23.08.0/poppler-23.08.0-clang-aarch64.tar.xz"
-  } else if(grepl("clang", Sys.getenv('R_COMPILED_BY'))){
+  } else if (grepl("clang", Sys.getenv("R_COMPILED_BY"))) {
     "https://github.com/pachadotdev/cpp11bundles/releases/download/poppler-23.08.0/poppler-23.08.0-clang-x86_64.tar.xz"
   } else {
     "https://github.com/pachadotdev/cpp11bundles/releases/download/poppler-23.08.0/poppler-23.08.0-ucrt-x86_64.tar.xz"
   }
   download.file(url, basename(url), quiet = TRUE)
   dir.create("../windows", showWarnings = FALSE)
-  untar(basename(url), exdir = "../windows", tar = 'internal')
+  untar(basename(url), exdir = "../windows", tar = "internal")
   unlink(basename(url))
   setwd("../windows")
-  file.rename(list.files(), 'poppler')
+  file.rename(list.files(), "poppler")
+}
+
+# Harfbuzz ----
+
+if (!file.exists("../windows/harfbuzz/include/harfbuzz/hb.h")) {
+  unlink("../windows/harfbuzz", recursive = TRUE)
+  url <- if (grepl("aarch", R.version$platform)) {
+    "https://github.com/pachadotdev/readpdf/releases/download/harfbuzz-8.2.1/harfbuzz-8.2.1-clang-aarch64.tar.xz"
+  } else if (grepl("clang", Sys.getenv("R_COMPILED_BY"))) {
+    "https://github.com/pachadotdev/readpdf/releases/download/harfbuzz-8.2.1/harfbuzz-8.2.1-clang-x86_64.tar.xz"
+  } else if (getRversion() >= "4.3") {
+    "https://github.com/pachadotdev/readpdf/releases/download/harfbuzz-8.2.1/harfbuzz-8.2.1-ucrt-x86_64.tar.xz"
+  }
+  download.file(url, basename(url), quiet = TRUE)
+  untar(basename(url), exdir = "../windows/", tar = "internal")
+  file.rename(file.path("../windows", dir("../windows", pattern = "harfbuzz-")), "../windows/harfbuzz")
+  unlink(basename(url))
 }
