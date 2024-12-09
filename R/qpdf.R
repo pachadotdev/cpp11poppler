9641dfb61019870187311f56632200b5f0e9837a
Mauricio 'Pacha' Vargas Sepulveda <m.sepulveda@mail.utoronto.ca>
2024-12-08 19:28:36 -0500

    document qpdf params

--- R/qpdf.R
+++ R/qpdf.R
@@ -3,6 +3,11 @@
 #' @name qpdf
 #' @title qpdf utilities
 #' @family cpp11poppler
+#' @param input A character vector of input PDF file paths.
+#' @param output The output PDF file path.
+#' @param password An optional password for encrypted PDFs.
+#' @param linearize Logical, whether to linearize the output PDF.
+#' @param pages A character vector specifying the pages to include.
 #' @export
 qpdf::pdf_combine
 
