310beb6e70d15b3b1e24fb70eb96448b0cb97981
Mauricio 'Pacha' Vargas Sepulveda <m.sepulveda@mail.utoronto.ca>
2024-12-09 00:14:39 -0500

    use old harfbuzz for R 4.1

--- tools/winlibs.R
+++ tools/winlibs.R
@@ -6,7 +6,7 @@ if (!file.exists("../windows/poppler/include/poppler/cpp/poppler-document.h")) {
     "https://github.com/pachadotdev/cpp11bundles/releases/download/poppler-23.08.0/poppler-23.08.0-clang-aarch64.tar.xz"
   } else if (grepl("clang", Sys.getenv("R_COMPILED_BY"))) {
     "https://github.com/pachadotdev/cpp11bundles/releases/download/poppler-23.08.0/poppler-23.08.0-clang-x86_64.tar.xz"
-  } else if(getRversion() >= "4.3") {
+  } else if (getRversion() >= "4.3") {
     "https://github.com/pachadotdev/cpp11bundles/releases/download/poppler-23.08.0/poppler-23.08.0-ucrt-x86_64.tar.xz"
   } else {
     "https://github.com/pachadotdev/cpp11bundles/releases/download/poppler-oldR-22.04.0-2/poppler-oldR-22.04.0-2.tar.gz"
@@ -25,8 +25,10 @@ if (!file.exists("../windows/harfbuzz/include/harfbuzz/hb.h")) {
     "https://github.com/pachadotdev/cpp11bundles/releases/download/harfbuzz-8.2.1/harfbuzz-8.2.1-clang-aarch64.tar.xz"
   } else if (grepl("clang", Sys.getenv("R_COMPILED_BY"))) {
     "https://github.com/pachadotdev/cpp11bundles/releases/download/harfbuzz-8.2.1/harfbuzz-8.2.1-clang-x86_64.tar.xz"
-  } else (getRversion() >= "4.3") {
+  } else if (getRversion() >= "4.3") {
     "https://github.com/pachadotdev/cpp11bundles/releases/download/harfbuzz-8.2.1/harfbuzz-8.2.1-ucrt-x86_64.tar.xz"
+  } else {
+    "https://github.com/pachadotdev/cpp11bundles/releases/download/harfbuzz-oldR-2.7.4/harfbuzz-oldR-2.7.4.tar.gz"
   }
   download.file(url, basename(url), quiet = TRUE)
   untar(basename(url), exdir = "../windows/", tar = "internal")
