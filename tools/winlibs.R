cdad950088899c848e96c7e09b7bf5099e466c88
Mauricio 'Pacha' Vargas Sepulveda <m.sepulveda@mail.utoronto.ca>
2024-12-08 22:20:22 -0500

    modify poppler bundle

--- tools/winlibs.R
+++ tools/winlibs.R
@@ -1,7 +1,7 @@
 # Poppler ----
 
 if (!file.exists("../windows/poppler/include/poppler/cpp/poppler-document.h")) {
-  unlink("../windows", recursive = TRUE)
+  unlink("../windows/poppler", recursive = TRUE)
   url <- if (grepl("aarch", R.version$platform)) {
     "https://github.com/pachadotdev/cpp11bundles/releases/download/poppler-23.08.0/poppler-23.08.0-clang-aarch64.tar.xz"
   } else if (grepl("clang", Sys.getenv("R_COMPILED_BY"))) {
@@ -12,9 +12,8 @@ if (!file.exists("../windows/poppler/include/poppler/cpp/poppler-document.h")) {
   download.file(url, basename(url), quiet = TRUE)
   dir.create("../windows", showWarnings = FALSE)
   untar(basename(url), exdir = "../windows", tar = "internal")
+  file.rename(file.path("../windows", dir("../windows", pattern = "poppler-")), "../windows/poppler")
   unlink(basename(url))
-  setwd("../windows")
-  file.rename(list.files(), "poppler")
 }
 
 # Harfbuzz ----
